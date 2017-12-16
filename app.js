11722509d5bd3f61cd6547b6cad36b262ffec8e7
Israel Martinez <iukones1@gmail.com>
2017-12-15 19:49:32 -0600

    cambios 2
    mas cambios en archivo de curso

--- app.js
+++ app.js
@@ -27,16 +27,46 @@ console.log(myVar);
 
 // cadena de alcance Scope, el entorno léxico.
 
-function b() {
-    console.log(myVar);
-}
+// function b() {
+//     console.log(myVar);
+// }
 
-function a() {
-    var myVar = 2;    
-    b();
-}
+// function a() {
+//     var myVar = 2;    
+//     b();
+// }
 
-var myVar = 1;
-a();
+// var myVar = 1;
+// a();
 
 // cadena de alcance Scope, el entorno léxico.
+
+//JavaScript y Undefined
+
+// var a;
+// console.log(a);
+
+// if (a === undefined) {
+//     console.log('a es Undefined');
+// }else{
+//     console.log('a no es Undefined');
+// }
+
+//JavaScript y Undefined
+
+// the execution context - Code execution
+
+function b() {
+    console.log('llamada funcion b!')
+}
+
+b();
+
+console.log(a);
+
+var a = 'Hola Mundo!';
+
+console.log(a);
+
+
+// the execution context - Code execution
