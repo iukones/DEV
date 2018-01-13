60aec71202c17c4d23bf3798cc36d27ec4fc5ba1
Israel Martinez <iukones1@gmail.com>
2018-01-12 19:20:25 -0600

    capitulo 27
    mas capitulo 27

--- app.js
+++ app.js
@@ -148,12 +148,65 @@ console.log(myVar);
 
 // Precedencia del operador y asociatividad.
 
-var a = 2, b = 3, c = 4;
+// var a = 2, b = 3, c = 4;
 
-a = b = c;
+// a = b = c;
 
-console.log(a);
-console.log(b);
-console.log(c);
+// console.log(a);
+// console.log(b);
+// console.log(c);
 
 // Precedencia del operador y asociatividad.
+
+//Coerción en JS
+
+// var a = '1' + 2;
+
+// console.log(a); //resultado sera "12"
+
+// var a = 1, b = '2';
+// console.log(a + b); //resultado sera igual "12"
+
+//Coerción en JS
+
+
+// Operadores de Comparación
+
+// console.log(1 < 2 < 3); // valor booleano "True"
+
+// console.log(3 < 2 < 1); // tambien devuelve "True" JS usa la coerción y forza a convertir el resultado booleano a un numero.
+//console.log(false < 1);
+
+// var a = 0;
+// var b = false;
+
+// if (a == b) {
+//     console.log('Son iguales!');
+// } else {
+//     console.log('Nop, no son iguales!');
+// }
+
+// Operadores de Comparación
+
+
+//existencia y booleanos
+
+// var a;
+
+// a = '';
+
+// if (a) {
+//     console.log('hay algo aquí'); //aparece si encuentra definido algun valor en "a".
+// }
+
+//existencia y booleanos
+
+// valores predeterminados
+
+function greet(name) {
+    console.log('Hola ' + name);
+}
+
+greet('Israel Martínez');
+
+// valores predeterminados
